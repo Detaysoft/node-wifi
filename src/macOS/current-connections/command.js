74764c273bfdbb7ef2ba4ca641523029145d72fe
Emre Güdür <emre.gudur@detaysoft.com>
2023-05-24 16:04:40 +0300

    getCurrent bssid problem fixed on macos

--- src/macOS/current-connections/command.js
+++ src/macOS/current-connections/command.js
@@ -1,6 +1,6 @@
 const command = () => ({
-  cmd: '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport',
-  args: ['--getinfo']
+  cmd: 'sudo',
+  args: ['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport', '--getinfo']
 });
 
 module.exports = command;
