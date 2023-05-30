4ee37acb747878265c8fb432fa60db4f374bbfb9
Emre Güdür <emre.gudur@detaysoft.com>
2023-05-30 14:15:43 +0300

    executer options changed

--- src/utils/executer.js
+++ src/utils/executer.js
@@ -1,12 +1,10 @@
-// const { execFile } = require('child_process');
+const { execFile } = require('child_process');
 const sudo = require('sudo-prompt');
-
 const env = require('../env');
 
 module.exports = ({ cmd, args }) =>
   new Promise((resolve, reject) => {
-    console.log(cmd, 'node wifi cmd');
-    sudo.exec(cmd, args, { env }, (error, output) => {
+    sudo.exec(`${cmd} ${args.join(' ')}`, {}, (error, output) => {
       if (error) {
         reject(error);
       } else {
